8392e2ba0e442ed39fb9499e80f9a4e6a37adaa3
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2025-11-28 18:51:34 +0100

    add alias to install hooks per directory

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset
 
+# Check if current directory is a Git repo by looking for .git
+if [ ! -d .git ]; then
+    echo "Error: This is not a Git repository."
+    exit 1
+fi
+
 # https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
 if command -v install-ticket-commit-msg-hook.sh &> /dev/null
 then
