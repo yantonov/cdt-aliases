359515349283c5b8541dd5001434aec97308d62a
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2026-08-08 01:18:05 +0200

    migrate to sh from bash

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/usr/bin/env sh
+set -eu
 
 cd "$(dirname "$0")"
 
 # https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
-command -v curl >/dev/null 2>&1 || { echo "I require curl but it's not installed.  Aborting." >&2; exit 1; }
+
+command -v curl > /dev/null 2>&1 || {
+    echo "I require curl but it's not installed.  Aborting." >&2
+    exit 1
+}
 
 APP_NAME="cdt"
 
-curl -fsSL "https://raw.githubusercontent.com/yantonov/alias/master/bin/download.sh" | bash -s -- "${APP_NAME}"
+curl -fsSL "https://raw.githubusercontent.com/yantonov/alias/master/bin/download.sh" | sh -s -- "${APP_NAME}"
