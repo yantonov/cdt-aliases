fa5806b8152450e3bc647791c0b56ad1575c21b5
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2026-08-08 01:16:52 +0200

    move to sh from bash

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -o errexit -o nounset
 
 # Check if we're inside a git repository
-if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+
+if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     # echo "Inside a Git repository."
     # git status
     ROOT_DIR=$(git rev-parse --show-toplevel)
@@ -16,7 +17,8 @@ fi
 # https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
 
 # if command -v install-ticket-commit-msg-hook.sh &> /dev/null
-if command -v krok &> /dev/null   
+
+if command -v krok > /dev/null 2>&1
 then
     # -install-ticket-commit-msg-hook.sh --force
     krok add commit-msg ticket-commit-msg
@@ -27,7 +29,8 @@ else
 fi
 
 # https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
-if command -v ticket-commit-msg &> /dev/null
+
+if command -v ticket-commit-msg > /dev/null 2>&1
 then
     git config custom.ticketnumberprefix 'JIRA: '
 else
